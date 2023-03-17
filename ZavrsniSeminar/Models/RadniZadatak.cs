5ca4d78f1f1615eda8bb4d51e596cdc59e1cbf64
gitcontrolsetings <misicratko.sicmi@gmail.com>
2023-03-17 11:05:03 +0100

    ZavrsniSeminar

---
+++ ZavrsniSeminar/Models/RadniZadatak.cs
@@ -0,0 +1,13 @@
+﻿namespace ZavrsniSeminar.Models
+{
+    public class RadniZadatak
+    {
+        public int Id { get; set; }
+        public string Todo { get; set; }
+        public string RadnikIme { get; set; }
+        public string RadnikPrezime { get; set; }
+        public string OpisPosla { get; set; }
+        public DateTime Datum { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
